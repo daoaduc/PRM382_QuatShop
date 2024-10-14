a41fed145e6d8025a27055674d549c9e7d7fbc2c
MAK4a <124328625+MAK4a@users.noreply.github.com>
2024-10-15 01:04:54 +0700

    cart_item-fix
    add background for delete

--- app/src/main/java/com/example/prm392/activity/User/CartFragment.java
+++ app/src/main/java/com/example/prm392/activity/User/CartFragment.java
@@ -3,11 +3,10 @@ package com.example.prm392.activity.User;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-public class CartFragment extends AppCompatActivity {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-    }
+public class CartFragment extends Fragment {
+
 }
