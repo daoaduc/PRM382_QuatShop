cb4f79870ec4f7b969a9b57ec371e1e9ce3d3903
MAK4a <124328625+MAK4a@users.noreply.github.com>
2024-10-15 01:01:00 +0700

    Icon in cart
    back,minus,plus,delete

---
+++ app/src/main/java/com/example/prm392/activity/User/CartFragment.java
@@ -0,0 +1,13 @@
+package com.example.prm392.activity.User;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CartFragment extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
