b70966640e8997877e5aa9c78cc0288216bb77a8
MAK4a <124328625+MAK4a@users.noreply.github.com>
2024-10-31 09:33:58 +0700

    add getProductById in dao

--- app/src/main/java/com/example/prm392/DAO/ProductDAO.java
+++ app/src/main/java/com/example/prm392/DAO/ProductDAO.java
@@ -56,6 +56,41 @@ public class ProductDAO {
         return productList;
     }
 
+    public Product getProductById(int productID){
+        Connection connection = connectionClass.CONN();
+
+        if (connection != null) {
+            // Thêm cột 'productIMG' để lấy tên hình ảnh
+            String query = "SELECT productName, price, quantity, description, productIMG FROM products WHERE productID = ?";
+            try {
+                PreparedStatement stmt = connection.prepareStatement(query);
+                stmt.setInt(1, productID);
+                ResultSet rs = stmt.executeQuery();
+
+                while (rs.next()) {
+                    Product product = new Product();
+                    product.setProductName(rs.getString("productName"));
+                    product.setQuantity(rs.getInt("quantity"));
+                    product.setPrice(rs.getLong("price"));
+                    product.setDescription(rs.getString("description"));
+
+                    // Lấy đường dẫn hình ảnh từ cột 'productIMG'
+                    product.setProductIMG(rs.getString("productIMG"));
+                    return product;
+                }
+
+                rs.close();
+                stmt.close();
+                connection.close();
+            } catch (Exception e) {
+                Log.e("ERROR", "Error while fetching products: " + e.getMessage());
+            }
+        } else {
+            Log.e("ERROR", "Connection to database failed");
+        }
+        return null;
+    }
+
     public List<ProductCategory> getAllCategories() {
         List<ProductCategory> categoriesList = new ArrayList<>();
         Connection connection = connectionClass.CONN();
